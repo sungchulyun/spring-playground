6829633729e91c658adf724dda37fbb279c34b57
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-28 17:09:03 +0900

    fix : disable kafka listener

--- src/main/java/com/example/springplayground/kafka/consumer/TestConsumer.java
+++ src/main/java/com/example/springplayground/kafka/consumer/TestConsumer.java
@@ -1,6 +1,7 @@
 package com.example.springplayground.kafka.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class TestConsumer {
 
-    @KafkaListener(topics = "exam-topic", groupId = "group_1")
+    @KafkaListener(topics = "exam-topic", groupId = "group_1", autoStartup = "${kafka.listener.myListenerMethod.enabled}")
     public void listener(Object data) {
       log.info("Consumed Message : {}", data);
     }
